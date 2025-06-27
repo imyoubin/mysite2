6a68459f852664fe2584beb7e05136ff0d6ce59d
clz2025-red <clz2025.red@gmail.com>
2025-06-27 12:59:29 +0900

    2025-06-27

---
+++ src/main/java/com/javaex/controller/UserController.java
@@ -0,0 +1,83 @@
+package com.javaex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaex.service.UserService;
+import com.javaex.vo.UserVO;
+
+@Controller
+public class UserController {
+
+	//필드
+	@Autowired
+	private UserService userService;
+	
+	//메소드일반
+	//--회원가입폼
+	@RequestMapping(value="/user/joinform", method = {RequestMethod.GET, RequestMethod.POST}   )
+	public String joinForm() {
+		System.out.println("UserController.joinForm()");
+		
+		return "user/joinForm";
+	}
+	
+	//--회원가입
+	@RequestMapping(value="/user/join", method = {RequestMethod.GET, RequestMethod.POST})
+	public String join(@ModelAttribute UserVO userVO) {
+		System.out.println("UserController.join()");
+		
+		try {
+			userService.exeJoin(userVO);
+			return "user/joinok";
+			
+		}catch(DuplicateKeyException e) {
+			System.out.println(e);
+			System.out.println("중복아이디");
+			return "redirect:/user/joinform";
+			
+		}catch (Exception e) {
+			System.out.println(e);
+			return "redirect:/user/joinform";
+		}
+		
+	}
+	
+	//--로그인폼
+	@RequestMapping(value="/user/loginform", method= {RequestMethod.GET, RequestMethod.POST})
+	public String loginForm() {
+		System.out.println("UserController.loginForm()");
+		
+		return "/user/loginForm";
+	}
+	
+	
+	//--로그인
+	@RequestMapping(value="/user/login", method= {RequestMethod.GET, RequestMethod.POST})
+	public String login(@ModelAttribute UserVO userVO) {
+		System.out.println("UserController.login()");
+		
+		userService.exeLogin(userVO);
+		
+		return "";
+	}
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/javaex/service/UserService.java
@@ -0,0 +1,41 @@
+package com.javaex.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaex.repository.UserRepository;
+import com.javaex.vo.UserVO;
+
+@Service
+public class UserService {
+	//필드
+	@Autowired
+	private UserRepository userRepository;
+
+	//메소드일반
+	//--회원가입
+	public int exeJoin(UserVO userVO){
+		System.out.println("UserService.exeJoin()");
+		
+		int count = userRepository.userInsert(userVO);
+		
+		return count;
+	}
+	
+	//--로그인
+	public void exeLogin(UserVO userVO) {
+		System.out.println("UserService.exeLogin()");
+
+		userRepository.userSelectOneByIdPw(userVO);
+	
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
