3d4239cb26ec3e386982d06e953769abb5c72b50
clz2025-red <clz2025.red@gmail.com>
2025-07-02 12:41:23 +0900

    2025-07-02

--- src/main/java/com/javaex/controller/MainController.java
+++ src/main/java/com/javaex/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,21 +17,23 @@ public class MainController {
 	//메소드일반
 	//--사이트 시작 페이지
 	@RequestMapping(value="/", method= {RequestMethod.GET, RequestMethod.POST})
-	public String index(@RequestParam(value="id") String id) {
+	public String index() {
 		System.out.println("MainController.index()");
-		
-		System.out.println(id);
-		System.out.println(id+"회원의 메인 사이트 정보를 가져와서 출력한다");
 		
 		return "main/index";
 	}
 	
-	@RequestMapping(value="/aaa", method= {RequestMethod.GET, RequestMethod.POST})
-	public String index() {
+	@RequestMapping(value="/{id}/{no}", method= {RequestMethod.GET, RequestMethod.POST})
+	public String index2(@PathVariable(value="id") String id,
+						 @PathVariable(value="no") int no
+			) {
 		System.out.println("MainController.index()");
 		
-		System.out.println("aaa 회원의 메인 사이트 정보를 가져와서 출력한다");
+		System.out.println(id);
+		System.out.println(id+ "회원의 메인 사이트 정보를 가져와서 출력한다");
 		
+		System.out.println(no);
+		System.out.println(no+ "회원의 메인 사이트 정보를 가져와서 출력한다");
 		return "main/index";
 	}
 	
